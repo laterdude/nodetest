e5f819594d1d6fe9235185df69a9e9558e116eff
laterdude <mathalistic@gmail.com>
2017-08-07 22:03:06 -0600

    update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 
 app.get('/dev', function(req, res) {
-	res.send('Hello from github part 2')
+	res.send('Hello from github auto-deploy')
 })
 
 app.listen(5678)
