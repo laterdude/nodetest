cd81c34a990188706eda174292a139c70b035381
laterdude <mathalistic@gmail.com>
2017-08-07 23:18:38 -0600

    go home ryan

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 
 app.get('/dev', function(req, res) {
-	res.send('I am deployed now')
+	res.send('I am deployed now ryan, go home')
 })
 
 app.listen(5678)
