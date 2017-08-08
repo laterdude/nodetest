d909361c1fcb924ccc97a227931f789e0632db93
laterdude <rtippetts@blm.gov>
2017-08-08 01:56:36 +0000

    test jenkins

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 var express = require('express')
 var app = express()
 
-app.get('/', function(req, res) {
-	res.send('Hello from github')
+app.get('/dev', function(req, res) {
+	res.send('Hello from github part 2')
 })
 
 app.listen(5678)
+console.log('Server running at http://localhost:5678/');
+// var http = require('http');
+// http.createServer(function (req, res) {
+//   res.writeHead(200, {'Content-Type': 'text/plain'});
+//   res.end('Hello from github');
+// }).listen(5678, 'localhost');
+// console.log('Server running at http://localhost:5678/');
