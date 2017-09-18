eb79c83f8a0862fb466e1633d309b7ed213b785e
laterdude <rtippetts@blm.gov>
2017-09-18 13:49:35 +0000

    last modification

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ var express = require('express')
 var app = express()
 
 app.get('/dev', function(req, res) {
-	res.send('testing 4567')
+	res.send('Server running at http://localhost:5678/, git scm polling is operational as of ' + new Date(Date.now()))
 })
 
 app.listen(5678)
-console.log('Server running at http://localhost:5678/, git scm polling is operational');
+// console.log('Server running at http://localhost:5678/, git scm polling is operational');
 // var http = require('http');
 // http.createServer(function (req, res) {
 //   res.writeHead(200, {'Content-Type': 'text/plain'});
